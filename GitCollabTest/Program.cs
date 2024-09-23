a77358f1b4581cf7721febab0827708d48e08087
Krelle156 <kotofteng@gmail.com>
2024-09-23 13:15:06 +0200

    Added carl the variable

--- GitCollabTest/Program.cs
+++ GitCollabTest/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Ha ha ha, gaffel!");
+
+
+String carl = "Karl the Jarl";
+Console.WriteLine(carl);
